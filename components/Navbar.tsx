aba32dd4d3a69478780f044e9c80212a9dedd325
Adam Ciołek <adasciolek95@gmail.com>
2022-07-04 10:28:19 +0200

    added shadow effect when we scroll

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,36 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useScroll } from "../hooks/useScroll";
+
+const classNames = (...classes: any) => {
+	return classes.filter(Boolean).join(" ");
+};
 
 const Navbar = () => {
+	const scrollPosition = useScroll();
+
 	return (
 		<header>
-			<nav className="flex justify-between max-w-screen-xl mx-auto pt-8 fixed top-0 left-0 right-0 items-center px-6 ">
-				<div>
-					<Image src="/logo.png" alt="Logo Food blog" width={271} height={35} />
-				</div>
-				<div className="space-x-10 text-lg text-zinc-700 font-openSans hidden md:flex">
-					<Link href="/">Home</Link>
-					<Link href="/about">About</Link>
-					<Link href="/contact">Contact</Link>
+			<nav
+				className={classNames(
+					scrollPosition > 0 ? "shadow" : "shadow-none",
+					" w-full py-8  fixed top-0 left-0 right-0  px-6 bg-white transition-shadow"
+				)}
+			>
+				<div className="max-w-screen-xl mx-auto flex justify-between items-center">
+					<div>
+						<Image
+							src="/logo.png"
+							alt="Logo Food blog"
+							width={271}
+							height={35}
+						/>
+					</div>
+					<div className="space-x-10 text-lg text-zinc-700 font-openSans hidden md:flex">
+						<Link href="/">Home</Link>
+						<Link href="/about">About</Link>
+						<Link href="/contact">Contact</Link>
+					</div>
 				</div>
 			</nav>
 		</header>
---
+++ hooks/useScroll.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const useScroll = () => {
+	const [scroll, setScroll] = useState(0);
+
+	useEffect(() => {
+		const position = () => setScroll(window.pageYOffset);
+
+		window.addEventListener("scroll", position);
+
+		position();
+
+		return () => window.removeEventListener("scroll", position);
+	}, []);
+
+	return scroll;
+};
