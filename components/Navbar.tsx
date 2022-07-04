c49464e79a10fb3e43dc812c878fa7838d3ccaa0
Adam Ciołek <adasciolek95@gmail.com>
2022-07-04 09:52:35 +0200

    added page 404

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 const Navbar = () => {
 	return (
 		<header>
-			<nav className="flex justify-between max-w-screen-xl mx-auto pt-8 fixed top-0 left-0 right-0 items-center">
+			<nav className="flex justify-between max-w-screen-xl mx-auto pt-8 fixed top-0 left-0 right-0 items-center px-6 ">
 				<div>
 					<Image src="/logo.png" alt="Logo Food blog" width={271} height={35} />
 				</div>
-				<div className="space-x-10 text-xl font-normal font-openSans">
+				<div className="space-x-10 text-xl font-normal font-openSans hidden md:flex">
 					<Link href="/">Home</Link>
 					<Link href="/about">About</Link>
 					<Link href="/contact">Contact</Link>
